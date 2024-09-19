f6c8feef69947012f38132c621be28cbf73a857d
allllleeeex <agepp27@gmail.com>
2024-09-19 13:32:21 +0000

    hi

--- Main.java
+++ Main.java
@@ -5,7 +5,10 @@ public class Main
         Feeder f = new Feeder();
         System.out.println(f);
         //System.out.println(f.currentFood);
-        System.out.println(f.getCurrentFood());
         Feeder g = new Feeder(500);
+        System.out.println(g.getCurrentFood());
+        if(Math.random() < 0.05) currentFood = 0;
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
     }
 }
